6e6d40082108d77f56f77b6c01507a47d2f40c31
Cory Spitzer <coryspitzer@gmail.com>
2018-05-04 10:17:14 -0400

    Add classes

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -5,18 +5,70 @@ class Board
                   ['_', '_', '_']]
     end
 
+    attr_accessor :tiles
+
     def place(x, y, mark) # x,y co-ordinates i.e. [1,2]
         @tiles[y, x] = mark
     end
 
-    def is_winner?
-
+    def is_winner?(player)
+        # Check the two diagonals:
+        if player.mark == @tiles[1][1]
+            if player.mark == @tiles[0][0] && player.mark == @tiles[2][2] ||
+               player.mark == @tiles[0][2] && player.mark == @tiles[2][0]
+                return true
+            end
+        # Check the top row or first column
+        elsif player.mark == @tiles[0][0]
+            # top row
+            if player.mark == @tiles[0][1] && player.mark == @tiles[0][2] ||
+               # first column
+               player.mark == @tiles[1][0] && player.mark == @tiles[2][0]
+                return true
+            end
+        # Check the bottom row or last column
+        elsif player.mark == @tiles[2][2]
+            # bottom row
+            if player.mark == @tiles[2][1] && player.mark == @tiles[2][0] ||
+               # last column
+               player.mark == @tiles[1][2] && player.mark == @tiles[0][2]
+                return true
+            end
+        else
+            false
+        end
     end
+
+    def print
+        (0..2).each do |i|
+            (0..2).each do |j|
+                puts @tiles[i][j] + ' '
+            end
+            puts
+        end
+    end
+end
+
+class Player
+    def initialize(mark) # such as 'x' or 'o'
+        @mark = mark
+    end
+
+    def take_turn(board)
+        # only place in unoccupied spot
+        # get move location
+        puts "Enter your move, 00 for the top left"
+        location = gets.chomp
+        board.tiles[location[1]][location[0]]
+    end
+
+    attr_reader :mark
 end
 
 def main()
     board = Board.new
     winner = false
+    current_player = 'x'
     while !winner
         current_player.take_turn
         winner = board.is_winner?
