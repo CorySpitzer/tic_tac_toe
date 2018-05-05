1674dc3318a41bc599e00aa0781da99d8d7d6ffd
Cory Spitzer <coryspitzer@gmail.com>
2018-05-05 13:26:59 -0400

    Arrange infrastructue for AI

--- lib/tic_tac_toe_lib.rb
+++ lib/tic_tac_toe_lib.rb
@@ -52,36 +52,49 @@ class Board
 end
 
 class Player
-    def initialize(mark) # such as 'x' or 'o'
+    def initialize(mark, is_ai) # such as 'x' or 'o'
         @mark = mark
+        @is_ai = is_ai
+    end
+
+    def ai_move(board)
+        board.tiles[0][0]
+    end
+
+    def ai_move
+
     end
 
     def take_turn(board)
         # only place in unoccupied spot
         # get move location
-        while true
-            vertical_down = -1
+        if @is_ai
+            ai_move
+        else # humans
             while true
-                puts "Vertical down 1..3"
-                vertical_down = gets.chomp.to_i - 1
-                if vertical_down >= 0 && vertical_down <= 2
+                vertical_down = -1
+                while true
+                    puts "Vertical down 1..3"
+                    vertical_down = gets.chomp.to_i - 1
+                    if vertical_down >= 0 && vertical_down <= 2
+                        break
+                    end
+                    puts "Try again"
+                end
+                while true
+                    puts "Horizontal across 1..3"
+                    horizontal_across = gets.chomp.to_i - 1
+                    if horizontal_across >= 0 && horizontal_across <= 2
+                        break
+                    end
+                    puts "Try again"
+                end
+                if board.is_occupied?(vertical_down, horizontal_across)
+                    puts "Square already taken, try again"
+                else
+                    board.tiles[vertical_down][horizontal_across] = @mark
                     break
                 end
-                puts "Try again"
-            end
-            while true
-                puts "Horizontal across 1..3"
-                horizontal_across = gets.chomp.to_i - 1
-                if horizontal_across >= 0 && horizontal_across <= 2
-                    break
-                end
-                puts "Try again"
-            end
-            if board.is_occupied?(vertical_down, horizontal_across)
-                puts "Square already taken, try again"
-            else
-                board.tiles[vertical_down][horizontal_across] = @mark
-                break
             end
         end
         board.print_board
@@ -91,5 +104,29 @@ class Player
 end
 
 class Game
+    def initialize()
+        # @is_ai = is_ai
+    end
 
+    def play(is_ai)
+        board = Board.new
+        # board.print_board
+        winner = false
+        player_x = Player.new('x', is_ai) #AI
+        player_o = Player.new('o', false)
+        # who goes first?
+        current_player = player_x
+        while !winner
+            current_player.take_turn(board)
+            winner = board.is_winner? current_player
+            if winner
+                puts "The " + current_player.mark + " player won!"
+            end
+            if current_player.mark == 'x'
+                current_player = player_o
+            else #if current_player.mark == 'o'
+                current_player = player_x
+            end
+        end
+    end
 end
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,36 +1,19 @@
 require_relative "lib/tic_tac_toe_lib"
 
 def main()
-    board = Board.new
-    # board.print_board
-    winner = false
-    player_x = Player.new('x')
-    player_o = Player.new('o')
-    current_player = player_x
     while true
         puts "Play against AI or just humans? 'h' for humans"
         if gets.chomp.downcase == 'h'
-            # Game.new.play('human')
+            Game.new.play('human')
         else
-            # Game.new.play('ai')
+            Game.new.play('ai')
         end
         puts "Press 'p' to play again"
         if gets.chomp.downcase != 'p'
             break
         end
     end
-    while !winner
-        current_player.take_turn(board)
-        winner = board.is_winner? current_player
-        if winner
-            puts "The " + current_player.mark + " player won!"
-        end
-        if current_player.mark == 'x'
-            current_player = player_o
-        else #if current_player.mark == 'o'
-            current_player = player_x
-        end
-    end
+
 end
 
 main()
